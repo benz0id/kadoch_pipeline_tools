2e446aec098be2a01a89cdc10dc87e5ac9cea41e
benz0id <bentudorprice@gmail.com>
2023-09-07 14:25:17 -0400

    *

--- utils/job_manager.py
+++ utils/job_manager.py
@@ -11,25 +11,6 @@ default_job = ExecParams(max_runtime=(0, 0, 10), num_cores=1,
                          ram_per_core=128, builder=JobBuilder())
 
 
-def combine_cmds(cmds: List[str], num_per: int) -> List[str]:
-    """
-    Divides the cmds into (len(cmds) // num_per + 1) combined cmds,
-    where each cmd contains at most <num_per> of the original commands.
-    :param cmds: A list of commands.
-    :param num_per: The number of cmds to assign to each job.
-    :return: A list of jobs that will execute all cmds.
-    """
-    combined_cmds = []
-    to_exec = []
-    for cmd in cmds:
-        to_exec.append(cmd)
-        if len(to_exec) == num_per:
-            combined_cmds.append('\n'.join(to_exec))
-            to_exec = []
-    if to_exec:
-        combined_cmds.append('\n'.join(to_exec))
-    return combined_cmds
-
 
 class JobManager:
     """
