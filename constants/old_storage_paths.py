06a6b1ad6c4f373ac4206812a23eb935e6741dad
benz0id <bentudorprice@gmail.com>
2023-09-18 13:10:59 -0400

    *

--- constants/old_storage_paths.py
+++ constants/old_storage_paths.py
@@ -5,8 +5,8 @@ ATAC_BEDS = Path("/home/bet488/kadoch/atac_fastq/beds")
 ATAC_STATS = Path("/home/bet488/kadoch/atac_fastq/stats_storage_atac")
 ATAC_BIGWIGS = Path("/home/bet488/kadoch/atac_fastq/bw")
 
-
-
-
-
+RNA_BAMS = Path("/home/bet488/kadoch/rna_fastq/bam")
+RNA_BEDS = Path("/home/bet488/kadoch/rna_fastq/beds")
+RNA_STATS = Path("/home/bet488/kadoch/rna_fastq/stats_storage_rna")
+RNA_BIGWIGS = Path("/home/bet488/kadoch/rna_fastq/bw")
 
