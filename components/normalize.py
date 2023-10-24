3f51532bfb18d7178c2be3665454b00b7e550bb2
benz0id <bentudorprice@gmail.com>
2023-10-23 22:46:13 -0400

    *

--- components/normalize.py
+++ components/normalize.py
@@ -1,13 +1,17 @@
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
+from utils.path_manager import cmdify
 from utils.utils import TargetedDesign
 
+DEFAULT_SUBSTRACT = '--subtract -bs 50 --smoothLength 600 --samFlagInclude 64'
 
-def normalise_bams(bams: List[Path],
-                   design: TargetedDesign,
-
-                   control_id: str = 'IgG') -> List[Path]:
+def subtract_background(bams: List[Path],
+                        design: TargetedDesign,
+                        out_dir: Path,
+                        args: str = DEFAULT_SUBSTRACT,
+                        control_id: str = 'IgG',
+                        num_cores: int = 1) -> Tuple[List[str], List[Path]]:
     bam_to_control = {}
 
     for bam in bams:
@@ -16,4 +20,32 @@ def normalise_bams(bams: List[Path],
         control = design.find_in_files(bams,
                                        treatments=[treatment],
                                        marks=[control_id], num_expected=1)[0]
+        bam_to_control[bam] = control
+
+    cmds = []
+    outfiles = []
+
+    for bam in bam_to_control:
+        control = bam_to_control[bam]
+
+        control_sample = control.name.split('_')[1]
+        outfile = out_dir / (bam.name[:-4] + 'sub' + control_sample + '.bw')
+
+        cmd = cmdify(
+            "bamCompare",
+            '--bamfile1', bam,
+            '--bamfile2', control,
+            '-o', outfile,
+            '--numberOfProcessors', num_cores,
+            args
+        )
+
+        cmds.append(cmd)
+        outfiles.append(outfile)
+
+    return cmds, outfiles
+
+
+
+
 
