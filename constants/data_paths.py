35eaf3d5b6884a8b21c4453765730dab1b4bc6a4
benz0id <bentudorprice@gmail.com>
2023-11-07 10:34:29 -0500

    *

--- constants/data_paths.py
+++ constants/data_paths.py
@@ -6,3 +6,5 @@ HG19_FASTQ = Path("/n/data1/dfci/pedonc/kadoch/anno/spike_in//hg19.refFlat.ercc.
 
 DUMMY_UNIVERSE = Path("/n/data1/dfci/pedonc/kadoch/ben/supporting_data/dummy_universe.bed")
 
+PIPELINE_BACKUP_DIR = Path("/n/data1/dfci/pedonc/kadoch/ben/pipeline_backpus")
+
