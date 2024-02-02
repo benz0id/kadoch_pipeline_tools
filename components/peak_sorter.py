dd4b6e968e580e1812b8c737c23b074c96a8e605
benz0id <bentudorprice@gmail.com>
2024-02-02 13:32:25 -0500

    *

--- components/peak_sorter.py
+++ components/peak_sorter.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import List
 
+import pandas as pd
+import numpy as np
+
 from components.peak_counter import PeakCounter
 from utils.utils import ExperimentalDesign
 
@@ -43,14 +46,24 @@ class PeakSorter:
         self._peak_counter.get_matrix(bed, self._counts_dir, self._design,
                                       matrix_out, samples)
 
+        matrix = pd.read_csv(matrix_out, sep='\t')
+        vals = matrix[samples].values
+        matrix[['row_sums']] = np.sum(vals, axis=0)
+        matrix.sort_values(by='row_sums', ascending=False)
+
+        with open(out_path, 'w') as out_file:
+            for peak in matrix[['Sites']]:
+                peak = peak.replace(':', '-')
+                contig, start, stop = peak.split('-')
+                out_file.write(f'{contig}\t{start}\t{stop}\n')
 
 
 
 
 
-
-    def comparison_sort(self, bed: Path, count1: Path, counts2: Path,
-                        out_path: Path, metric: str = 'fold_change') -> Path:
+    def comparison_sort(self, bed: Path, samples1: List[str],
+                        samples2: List[str], out_path: Path,
+                        metric: str = 'fold_change') -> Path:
         """
         Sorts <bed> according to some feature of
         :param bed:
@@ -60,4 +73,22 @@ class PeakSorter:
         :param metric:
         :return:
         """
+
+        matrix_out = out_path.parent / ('.'.join(out_path.name.split('.')[:-1])
+                                        + '.tsv')
+
+        self._peak_counter.get_matrix(bed, self._counts_dir, self._design,
+                                      matrix_out, samples1 + samples2)
+        matrix = pd.read_csv(matrix_out, sep='\t')
+
+        if metric == 'fold_change':
+            v1 = matrix[samples1].values.sum(axis=0)
+            v2 = matrix[samples2].values.sum(axis=0)
+            matrix[['fold_change']] = v2 / v1
+
+        with open(out_path, 'w') as out_file:
+            for peak in matrix[['Sites']]:
+                peak = peak.replace(':', '-')
+                contig, start, stop = peak.split('-')
+                out_file.write(f'{contig}\t{start}\t{stop}\n')
 
